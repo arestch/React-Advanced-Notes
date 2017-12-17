03117d8b9d64088df2665b11896c0ee996adc897
arestch <invfalle@gmail.com>
2017-12-17 19:40:02 +0300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "style.min.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
 
 module.exports = {
 	entry: './src/index.js',
@@ -13,22 +19,33 @@ module.exports = {
 	devtool: NODE_ENV === 'development' && 'eval-source-map',
 	watch: NODE_ENV === 'development',
 	module: {
-		loaders: [{
+		rules: [{
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/,
-			loader: ['react-hot-loader/webpack', 'babel-loader']
+			use: {
+          loader: "babel-loader"
+        }
 		},
 		{
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
-    }
-   ]
+			test: /\.scss$/,
+			use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+              })
+		}
+ ]
 	},
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
 				'NODE_ENV': JSON.stringify(NODE_ENV)
 			}
-		})
+		}),
+		extractSass
 	]
 };
