ea42c5e71db9d613a2287c3777f03b0668cc45cc
arestch <invfalle@gmail.com>
2017-12-17 14:35:34 +0300

    Note changed

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './Note.scss';
 
 class Note extends Component {
 
-	render() {
+	static propTypes = {
+		id: PropTypes.number.isRequired,
+		title: PropTypes.string.isRequired,
+		text: PropTypes.string
 
+	}
+
+	render() {
+		const { title, text} = this.props;
 		return (
 				<div className="notes-list__note">
-				<h4 className="notes-list__title"> Note! </h4>
-					<p className="notes-list__text">+ Note text! </p>
+				<h4 className="notes-list__title">{title}</h4>
+					{{text} &&
+					<p className="notes-list__text">{text}</p>}
 				</div>
 			);
 	}
